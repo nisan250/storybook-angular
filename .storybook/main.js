df588cdc7645db0b3c96c497778d2f70076d953d
nisan250 <nisan250@gmail.com>
2020-02-11 22:54:25 +0000

    adding addons and coonfiguration + adding theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-viewport',
+    '@storybook/addon-notes',
+    '@storybook/addon-docs',
+    // '@storybook/addon-storysource',
+    // '@storybook/addon-a11y/register',
+  ],
 };
